2bd25ecb05e22b88937b40aa20680b5c0041a4dc
fleazhang <130107092+fleazhang@users.noreply.github.com>
2023-11-01 22:42:21 -0700

    Create index.js
    this file is WIP but it working on connecting MongoDB atlas to code!

---
+++ index.js
@@ -0,0 +1,23 @@
+//connecting to database config code extends to 
+const express = require ('express');
+const config = require ('./config');
+const cors = require('cors');
+const router = require ("./api");
+const mongoose = require ('mongoose'); 
+
+server.use(cors()); 
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use('/api' router); 
+
+//remember to download mongoose! type into terminal: npm install mongoose
+mongoose.connect(config.databaseUrl, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true }).then(() => {
+  console.log('Connected to MonogoDB database');
+});
+
+server.listen(config.PORT, () => {
+    console.log('Server started on port ' + config.PORT);
+});
