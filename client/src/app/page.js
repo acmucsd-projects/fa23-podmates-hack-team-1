d0736abaa480bd45866ce3c84e1a8cc0b2c5a49c
Jake Villasenor <jrvillasenor@ucsd.edu>
2023-11-09 15:57:16 -0800

    Import LogInSignUp from pages to page.js

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import ChatInbox from './pages/ChatInbox'
+import LogInSignUp from './pages/SignIn'
+
 export default function Home() {
   return (
     <main>
