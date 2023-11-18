bb4ddff443ff6640faf0d887e9c0059145902d0e
Jake Villasenor <jrvillasenor@ucsd.edu>
2023-11-17 17:37:20 -0800

    Update page.js

--- client/src/app/forgot-password/page.js
+++ client/src/app/forgot-password/page.js
@@ -1,6 +1,24 @@
 "use client"
 
 export default function ForgotPassword() {
+    
+      // Whenever an input changes value, change the corresponding state variable
+    // const handleInputChange = async (e) => {
+    //     e.preventDefault();
+    //     const target = e.target;
+    //     this.setState({
+    //         [target.name]: target.value,
+    //     });
+    // }
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     if (this.state.password !== this.state.passwordVertify){
+    //         return;
+    //     }
+    // }
+
+
     return(
         <div>
             <h1>Forgot Password?</h1>
