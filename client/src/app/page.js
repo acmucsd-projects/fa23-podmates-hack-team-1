59f31aedcca19190465d8f5afd8b4f778aa0a41f
sji005 <siji@ucsd.edu>
2023-11-21 20:58:19 -0800

    Update page.js

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,19 @@
 "use client"
-import styles from './page.module.css'
+import styles from './page.module.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 export default function App() {
+const [user, setUser] = useState({});
+useEffect(() => {
+  async() => {
+    axios.get("http://localhost:5000/isAuth")
+    .then((response) =>{
+      console.log(response.user);
+    })
+    
+    
+  }
+})
   return (
     <main>
         <h1>Home Page</h1>
